cfd2dc7296ac97d46fc2264664c060476a5ce296
1518343069@qq.com <1518343069@qq.com>
2021-11-30 23:07:21 +0800

    切片是个类型 与数组的差距就在有没有长度。长度，容量 等。有点意思

--- Array/index.go
+++ Array/index.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	//Go 语言中的数组是一种 值类型  可以new
 	var nums [10]int
 	var nums2 = [10]int{10, 20, 7: 70} // 初始化  固定位置赋值
 	var nums3 = [...]int{10, 20, 30}   // 自动判断大小
 
-	nums4 := [3]int{1, 2, 3}
+	nums4 := [3]int{1, 2, 3} // 数组常量 也成为数组初始化
 	nums5 := [3]int{1, 2, 4}
 	nums6 := [3]int{1, 2, 3}
 	fmt.Println(nums, nums[1], nums2, nums3, nums4)
@@ -15,6 +16,8 @@ func main() {
 	fmt.Println("可以直接数组比较？ 离谱")
 	fmt.Println(nums4 == nums5)
 	fmt.Println(nums4 == nums6)
+	fmt.Println(&nums4 == &nums6)
+	fmt.Printf(" %p  %p\n", &nums6, &nums4)
 	fmt.Println(len(nums4))
 
 	// 便利
@@ -25,4 +28,21 @@ func main() {
 	for index, value := range nums4 {
 		fmt.Println(index, value)
 	}
+
+	fmt.Println("切片出来是个切片类型")
+	fmt.Printf("%T", nums2[0:2])      //[]int  不是数组 数组有长度
+	fmt.Printf("%v \n", nums2[0:2:3]) // 3是 指的是容量
+
+	var marrays [3][2]int
+	marrays2 := [3][2]int{{1, 2}, {2, 3}, {3, 4}}
+	fmt.Println("多维数组", marrays, marrays2)
+
+	//
+	fmt.Println("Go 语言中的数组是一种 值类型  可以new")
+	var arr1 = new([5]int) // arr1应该是一个指针1
+	arr2 := *arr1
+	arr2[2] = 100
+	fmt.Printf("%T %T\n", arr1, arr2)           //*[5]int [5]int
+	fmt.Printf("%p %p %p\n", arr1, arr2, &arr2) //*[5]int [5]int
+	fmt.Println(arr1, arr2)
 }
---
+++ slice/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("切片相关知识")
+	fmt.Println("声明切片的格式是： var name []type（不需要说明长度） 未初始化之前默认为 nil，长度为 0  计算容量的函数 cap()")
+
+	nums := []int{1, 2, 3}
+	fmt.Printf("相应值的go语法表示%#v  相应值 %v\n", nums, nums)
+
+	var arr1 [6]int
+	var slice1 []int = arr1[2:5] // 截取数组的地址  数组的切片也是切片类型
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	slice2 := arr2[1:]
+	fmt.Println("len和 cap都一样", arr2, slice2, len(arr2), len(slice2), cap(slice2))
+
+	// 给数组赋值
+	for i := 0; i < len(arr1); i++ {
+		arr1[i] = i
+	}
+
+	for i := 0; i < len(slice1); i++ {
+		fmt.Printf("Slice 第 %d位是 %d\n", i, slice1[i])
+	}
+	fmt.Printf("arr的长度是 %d\n", len(arr1))
+	fmt.Printf("slice1的长度是 %d\n", len(slice1))
+	// 这下面可能不太好理解  就是 切片的容量  =  数组长度 - 左指针走过的数（也就是start）
+	fmt.Printf("slice1的容量是 %d  切片的长度永远不会超过它的容量   0 <= len(s) <= cap(s)  \n", cap(slice1))
+	fmt.Println("因为容量 > len，所以可以调用append方法增加长度 append 返回一个 切片", append(slice1, 10))
+	fmt.Println("长度没有增加，因为没有赋值", slice2, len(slice1), cap(slice1))
+	fmt.Println("如果多次append 多次重新赋值 超出的本身容量的上线，会根据一个算法自动扩容 1-1.5倍之间")
+
+}
