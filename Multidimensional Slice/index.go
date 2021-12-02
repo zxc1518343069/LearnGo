9eed79205c77b1763c412c00ffdc5a29b10de8a6
1518343069@qq.com <1518343069@qq.com>
2021-12-02 23:49:52 +0800

    2021.12.2 多维指针。原理相通，不是不能存储不同类型的值。看来是我想错了。感觉是把很多C的东西带了过来。嗯。。 受制于js，感觉很多东西有点混淆了

--- Multidimensional Slice/index.go
+++ Multidimensional Slice/index.go
@@ -8,9 +8,17 @@ func main() {
 	slice2 := make([][]int, 0)
 	fmt.Println(slice, slice2)
 
-	// 没啥学的  基本原理相通
 	slice = append(slice, []int{1, 2, 3})
+	slice[0][0] = 2
+	slice[0] = make([]int, 2)
 
-	fmt.Println(slice, slice2)
+	fmt.Printf("%T \n", make([]int, 2)) // []int
+
+	fmt.Println(slice)
+
+	slice3 := [][]string{}
+	slice3 = append(slice3, []string{"123", "23234"})
+
+	fmt.Println(slice, slice2, slice3)
 
 }
